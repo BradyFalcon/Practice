cd0c9a1af139568bdb6b62cd6ff6ffe960ad6dc9
BradyF <brady.falcon@revature.net>
2021-10-24 12:01:00 -0500

    Practice

--- com/revature2/Com/src/LinkedListPractice.java
+++ com/revature2/Com/src/LinkedListPractice.java
@@ -1,4 +1,27 @@
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+
+
 public class LinkedListPractice {
 
-    "Hello"
-}
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        LinkedList<Character> linkedList = new LinkedList<>();
+        for (char i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+                linkedList.add(s.charAt(i));
+                System.out.println(linkedList);
+            }
+            }
+        }
+    }
+
+
+
