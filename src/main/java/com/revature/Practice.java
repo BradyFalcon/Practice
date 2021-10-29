b2b32e4a49ac6b9b751f25cf6429746d54c15151
BradyF <brady.falcon@revature.net>
2021-10-28 19:55:02 -0500

    Practice

---
+++ src/main/java/com/revature/Practice.java
@@ -0,0 +1,20 @@
+package com.revature;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Practice {
+    public static void main(String[] args) {
+
+
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> evens =
+                integers
+                        .stream()
+                        .filter(n -> n % 2 == 0)
+                        .distinct()
+                        .collect(Collectors.toList());
+        System.out.println(evens);
+    }
+}
