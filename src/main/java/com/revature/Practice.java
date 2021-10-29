38897a78e3d393a0d098e0c1be9352b6437e0fb4
BradyF <92339170+BradyFalcon@users.noreply.github.com>
2021-10-28 19:58:48 -0500

    Update Practice.java

--- src/main/java/com/revature/Practice.java
+++ src/main/java/com/revature/Practice.java
@@ -8,7 +8,7 @@ public class Practice {
     public static void main(String[] args) {
 
 
-        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9,10);
         List<Integer> evens =
                 integers
                         .stream()
